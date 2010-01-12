368cea9ca3ff19f9e0d7c08113abf4debd15f8f4
Fabrizio Regini <freegenie@gmail.com>
2010-01-12 17:56:23 +0100

    Added backup and restore, new version 0.0.3

--- lib/txp_admin.rb
+++ lib/txp_admin.rb
@@ -3,7 +3,60 @@ module TxpAdmin
   class ConfigNotFound < Exception
   end
   
-  
+  class RestoreFileNotFound < Exception 
+  end
+    
+  class MysqlConnect 
+    
+    @@config = nil 
+        
+    def self.config 
+      return @@config unless @@config.nil? 
+      @@config = TxpAdmin::Config.new.options       
+      # puts @@config.inspect
+      # FIXME: how do a mysql alias evaluated in exec? 
+      # Example: mysql='mysql --socket=/Applications/xampp/xamppfiles/var/mysql/mysql.sock'    
+      @@config[:host] = @@config[:host] == "localhost" ? "127.0.0.1" : @@config[:host]        
+      @@password = @@config[:pass].strip != "" ? " -p#{@@config[:pass]}" : ""
+      @@config
+    end
+    
+    def self.dump_command
+      c = "mysqldump #{self.command_options}"       
+    end    
+    
+    def self.backup 
+      command = "#{self.dump_command} | gzip -c > txpadmin_backup_#{Time.now.to_i}.dump.gz"
+      puts "executing: #{command}"      
+      exec(command)
+    end
+    
+    def self.restore(filename) 
+      raise RestoreFileNotFound unless File.exists?(filename)      
+      # "Restoring file #{filename}. Continue? [Y/n] "
+      command = "gunzip -c #{filename} | #{self.commandline}"
+      exec(command)
+    end
+    
+    def self.command_options 
+      " -u#{self.config[:user]} #{@@password} -h#{self.config[:host]} #{self.config[:db]}"
+    end
+    
+    def self.commandline 
+      c = "mysql #{self.command_options}"
+    end
+    
+    def self.execute(sql_command)      
+      puts "executing SQL: #{sql_command}"
+      exec("#{commandline} -e \"#{sql_command}\" ")
+    end
+    
+    def self.console       
+      # puts "executing command: #{commandline}"          
+      exec(commandline)
+    end
+    
+  end
   
   class Config 
     
@@ -13,8 +66,7 @@ module TxpAdmin
       @options = self.class.parse_config_file      
     end
 
-    def self.parse_config_file(path=".")      
-    
+    def self.parse_config_file(path=".")          
       config_file = "#{path}/textpattern/config.php"      
       raise TxpAdmin::ConfigNotFound unless File.exists?(config_file)            
       config = `cat #{config_file}`
@@ -22,13 +74,11 @@ module TxpAdmin
       config.gsub!("<?php", "")
       config.gsub!("?>", "")
       config.gsub!("$txpcfg", "txpcfg")
-      eval(config)      
-      
+      eval(config)            
       txpcfg.each do |key,value|
         txpcfg[key.to_sym] = value
       end  
-      
-      
+            
       # puts txpcfg.inspect
       return txpcfg      
     end
